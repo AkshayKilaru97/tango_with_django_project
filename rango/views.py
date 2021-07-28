37e945071d1e2340c77ec121266883534a6f3dec
AkshayKilaru97 <2589447K@student.gla.ac.uk>
2021-07-28 15:35:17 +0100

    forgot to add git ignore and completed chapter3

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    forward = "<a href = '/rango/about/'>About</a>"
     return HttpResponse("Rango says hey there partner!")
+
+def about(request):
+    backward = "<a href = '/rango/'>back</a>"
+    return HttpResponse("Rango says here is the about page!" + backward)
