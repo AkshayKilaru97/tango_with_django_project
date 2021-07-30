74290e672e9f42080b08947728fe732200e1a5d7
AkshayKilaru97 <2589447K@student.gla.ac.uk>
2021-07-30 15:30:10 +0100

    some minor bug fix

--- rango/views.py
+++ rango/views.py
@@ -25,7 +25,12 @@ def index(request):
     return render(request,'rango/index.html', context = context_dict)
 
 def about(request):
-    return render(request,'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
+    
 
 def show_category(request, category_name_slug):
     context_dict = {}
